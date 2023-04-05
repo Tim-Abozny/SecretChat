bcc64c1119c3fd36abcdbbbd5d700537b70b4a56
bumblebee <tim.obozny@gmail.com>
2023-04-05 11:10:47 +0300

    Upload pre-work version of chat

---
+++ courseProject/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace courseProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StartServer(object sender, RoutedEventArgs e)
+        {
+            ServerWindow server = new ServerWindow();
+            server.Show();
+        }
+        private void StartServerButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                StartServerEffects.ShadowDepth = -i;
+                StartServerEffects.BlurRadius = i;
+            }
+            StartServerEffects.Color = Color.FromRgb(92, 227, 80);
+        }
+
+        private void StartServerButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            for (int i = 20; i > 0; i--)
+            {
+                StartServerEffects.ShadowDepth = -i;
+                StartServerEffects.BlurRadius = i;
+            }
+            StartServerEffects.Color = Color.FromRgb(0, 0, 0);
+        }
+        private void StartClient(object sender, RoutedEventArgs e)
+        {
+            ClientWindow client = new ClientWindow();
+            client.Show();
+        }
+
+        private void StartClientButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                StartClientEffects.ShadowDepth = -i;
+                StartClientEffects.BlurRadius = i;
+            }
+            StartClientEffects.Color = Color.FromRgb(92, 227, 80);
+        }
+
+        private void StartClientButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            for (int i = 20; i > 0; i--)
+            {
+                StartClientEffects.ShadowDepth = -i;
+                StartClientEffects.BlurRadius = i;
+            }
+            StartClientEffects.Color = Color.FromRgb(0, 0, 0);
+        }
+    }
+}
---
+++ courseProject/ServerWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace courseProject
+{
+    /// <summary>
+    /// Interaction logic for ServerWindow.xaml
+    /// </summary>
+    public partial class ServerWindow : Window
+    {
+        public ServerWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 1234);
+            listener.Start();
+            //Console.WriteLine("Server started");
+            Info.Text = "Server started\n";
+
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                //Console.WriteLine("Client connected");
+                Info.Text = "Client connected\n";
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] encryptedData = new byte[1024];
+                int bytes = stream.Read(encryptedData, 0, encryptedData.Length);
+
+                byte[] decryptedData = ProtectedData.Unprotect(
+                    encryptedData,
+                    null,
+                    DataProtectionScope.LocalMachine);
+
+                string message = Encoding.UTF8.GetString(decryptedData, 0, bytes);
+                //Console.WriteLine("Received message: {0}", message);
+                Info.Text = "Received message: " + message + "\n";
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes("Message received");
+                byte[] responseEncrypted = ProtectedData.Protect(
+                    responseBytes,
+                    null,
+                    DataProtectionScope.LocalMachine);
+
+                stream.Write(responseEncrypted, 0, responseEncrypted.Length);
+
+                client.Close();
+                //Console.WriteLine("Client disconnected");
+                Info.Text = "Client disconnected\n";
+
+            }
+        }
+    }
+}
